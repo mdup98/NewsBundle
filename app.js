5f7da34445ee79ca430e118c284d3e998c2bf07e
mdup98 <mdup98@protonmail.com>
2021-01-18 22:09:47 +0100

    Frontend dropdown language toggle

--- app.js
+++ app.js
@@ -34,4 +34,9 @@ app.get("/articles/update", (req, res) => {
 	updateArticles();
 	res.end();
 });
+
+app.get("/articles/language/:param1", function (req, res) {
+	0;
+});
+
 app.listen(port, () => console.log(`Server listening on port ${port}`));
